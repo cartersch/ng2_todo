8f4692cdb4b6d314bd60eb2ea93a39c75627ece4
Carter Schoenfeld <carter.schoenfeld@gmail.com>
2016-09-25 11:14:42 -0400

    built basic list component

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,13 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoDataService } from '../todo-data.service';
 
 @Component({
-  selector: 'app-todo-list',
+  selector: 'todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  todos = [];
+
+  constructor(private todoService : TodoDataService) {
+    this.todos = this.todoService.getTodos();
+   }
+
+   updateTodoStatus(todo){
+     todo.done = !todo.done;
+   }
 
   ngOnInit() {
   }
