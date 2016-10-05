c634fb4f5888811532d1c2272334fdd1cc0a138b
Carter Schoenfeld <carter.schoenfeld@gmail.com>
2016-10-04 21:37:14 -0400

    adding a simple id creator and delete method

--- src/app/todo-data.service.ts
+++ src/app/todo-data.service.ts
@@ -7,12 +7,24 @@ export class TodoDataService {
 
   todos = [];
 
+  private createID(){
+    return Math.floor(Math.random() * 5000);
+  }
+
   addTodo(todo){
+    todo['id'] = this.createID();
     this.todos.push(todo);
   }
 
   getTodos(){
     return this.todos;
+  }
+
+  deleteTodo(id){
+    let remove = this.todos.map( (item) => { return item.id})
+                           .indexOf(id);
+
+    this.todos.splice(remove, 1);                     
   }
 
 }
